46e782a1e8642c14d059fe90295758f9c33858c9
João Victor Vieira Paes <joaovictorvp1313@gmail.com>
2018-09-12 13:47:58 -0300

    Adicionando template e criando repositório.

---
+++ Implastic/DotNet/Implastic.Testes/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Implastic.Testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PaisesDAOAdicionar()
+        {
+
+            //ProjetoBanca.DAO.PaisesDAO dao = new ProjetoBanca.DAO.PaisesDAO();
+            //ProjetoBanca.Models.Paises paises = new ProjetoBanca.Models.Paises();
+            //paises.Nome = "BRASIL";
+            //dao.Adiciona(paises);
+
+            //var p = dao.Lista(); 
+
+        }
+
+    }
+}
